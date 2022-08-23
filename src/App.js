b379d3f620cdf3dfa286aa8703877dd1247450f4
yassin0711 <99025891+yassin0711@users.noreply.github.com>
2022-08-23 13:24:10 +0100

    check state

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import React , {Component} from "react";
+
+let Person ={ fullName : "yassin louhichi",bio, imgSrc, profession}
+class App extends React.Component{
+  render(){
+  return (<h1>{this.Person.fullName}</h1>)
+}
+} 
+
+
+export default App;
